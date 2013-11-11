58f1d16f8c1cbb049567b2ec155fa6f1c7ef34d2
ShawnHardwick <swhardwi@g.coastal.edu>
2013-11-11 16:54:49 -0500

    Start fresh. Leap motion sample code restructured. Compiles fine.

---
+++ Wrapper Project/LeapDef.h
@@ -0,0 +1,21 @@
+/******************************************************************************\
+* Copyright (C) 2012-2013 Leap Motion, Inc. All rights reserved.               *
+* Leap Motion proprietary and confidential. Not for distribution.              *
+* Use subject to the terms of the Leap Motion SDK Agreement available at       *
+* https://developer.leapmotion.com/sdk_agreement, or another agreement         *
+* between Leap Motion and you, your company or other organization.             *
+\******************************************************************************/
+
+#include "Leap.h"
+using namespace Leap;
+
+class SampleListener : public Listener {
+  public:
+    virtual void onInit(const Controller&);
+    virtual void onConnect(const Controller&);
+    virtual void onDisconnect(const Controller&);
+    virtual void onExit(const Controller&);
+    virtual void onFrame(const Controller&);
+    virtual void onFocusGained(const Controller&);
+    virtual void onFocusLost(const Controller&);
+};
